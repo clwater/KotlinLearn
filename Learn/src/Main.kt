87d0fceca15c477b6346a9a4fa15662eebae7aab
clwater <gengzhibo@gengzhibo.net>
2017-05-21 23:37:38 +0800

    20170521

---
+++ Learn/src/Main.kt
@@ -0,0 +1,36 @@
+/**
+ * Created by yszsyf on 2017/5/21.
+ */
+
+//fun sum(a:Int , b: Int):Int {
+//    return a + b
+//}
+//
+//fun main(args: Array<String>){
+//    print("sum 3 and 5 is ")
+//    println(sum(3 ,5))
+//}
+//
+
+
+//fun sum(a:Int , b:Int) = a + b
+//fun main(args: Array<String>){
+//    println("sum 3 and 5 is ${sum(3 ,5)}")
+//}
+
+//fun printSum(a:Int , b : Int):Unit{
+//    println("sum of $a and $b is ${a + b}")
+//}
+//fun main(args: Array<String>){
+//    printSum(-1 , 8)
+//}
+
+
+
+fun printSum(a : Int , b : Int){
+    println("sum of $a and $b is ${a + b}")
+}
+
+fun main(args: Array<String>){
+    printSum(-2 , 6)
+}
---
+++ Learn/src/Main_2.kt
@@ -0,0 +1,18 @@
+/**
+ * Created by yszsyf on 2017/5/21.
+ */
+
+//fun main(args: Array<String>){
+//    var a : Int = 1
+//    var b = 2
+//    var c : Int
+//    c = 3
+//
+//    println("a = $a , b = $b , c = $c")
+//}
+
+fun main(args: Array<String>) {
+    var x = 5
+    x += 1
+    print("x = $x")
+}
